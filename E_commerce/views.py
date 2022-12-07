20d9ed9763e95c91a7a20a4ca17cc9355865ca9a
Apurba0012 <apurbadatta7331@gmail.com>
2022-12-08 01:21:18 +0600

    product add

--- E_commerce/views.py
+++ E_commerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.models import Category
+from app.models import Category,Products
 
 def master(request):
     return render(request, 'master.html')
@@ -7,8 +7,9 @@ def master(request):
 
 def index(request):
     category = Category.objects.all() #all category fetch from database
-
+    product = Products.objects.all()
     context = {
-        'category' : category  #context dict 'category' access through html
+        'category' : category,  #context dict 'category' access through html
+        'product'  : product,
     }
     return render(request, 'index.html',context)
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=150)
+
     def __str__(self):    #override
         return self.name
 
@@ -11,5 +12,18 @@ class Sub_Category(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=150)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    image = models.ImageField(upload_to='ecommerce/pimg')
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(Sub_Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
